6e859aaa749c40f823b2248af82327655bb79f3a
elyahu94-web <elyahu94@gmail.com>
2026-03-18 13:18:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tasks-pwa-v23';
+const CACHE = 'tasks-pwa-v24';
 const ASSETS = [
   './index.html',
   'https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&display=swap'
